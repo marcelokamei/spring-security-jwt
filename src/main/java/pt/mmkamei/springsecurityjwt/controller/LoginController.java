724175ffb2fab757a71d2b0346276195c766fdc6
marcelokamei <marcelokamei@hotmail.com>
2022-08-18 01:35:47 +0100

    Adicionando segurança em uma API REST com Spring Security e JWT

---
+++ src/main/java/pt/mmkamei/springsecurityjwt/controller/LoginController.java
@@ -0,0 +1,49 @@
+package pt.mmkamei.springsecurityjwt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pt.mmkamei.springsecurityjwt.dto.Login;
+import pt.mmkamei.springsecurityjwt.dto.Sessao;
+import pt.mmkamei.springsecurityjwt.model.User;
+import pt.mmkamei.springsecurityjwt.repository.UserRepository;
+import pt.mmkamei.springsecurityjwt.security.JWTCreator;
+import pt.mmkamei.springsecurityjwt.security.JWTObject;
+import pt.mmkamei.springsecurityjwt.security.SecurityConfig;
+
+import java.util.Date;
+@RestController
+public class LoginController {
+    @Autowired
+    private PasswordEncoder encoder;
+    @Autowired
+    private SecurityConfig securityConfig;
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/login")
+    public Sessao logar(@RequestBody Login login){
+        User user = repository.findByUsername(login.getUsername());
+        if(user!=null) {
+            boolean passwordOk =  encoder.matches(login.getPassword(), user.getPassword());
+            if (!passwordOk) {
+                throw new RuntimeException("Senha inválida para o login: " + login.getUsername());
+            }
+            //Estamos enviando um objeto Sessão para retornar mais informações do usuário
+            Sessao sessao = new Sessao();
+            sessao.setLogin(user.getUsername());
+
+            JWTObject jwtObject = new JWTObject();
+            jwtObject.setIssuedAt(new Date(System.currentTimeMillis()));
+            jwtObject.setExpiration((new Date(System.currentTimeMillis() + SecurityConfig.EXPIRATION)));
+            jwtObject.setRoles(user.getRoles());
+            sessao.setToken(JWTCreator.create(SecurityConfig.PREFIX, SecurityConfig.KEY, jwtObject));
+            return sessao;
+        }else {
+            throw new RuntimeException("Erro ao tentar fazer login");
+        }
+    }
+}
+
---
+++ src/main/java/pt/mmkamei/springsecurityjwt/service/UserService.java
@@ -0,0 +1,22 @@
+package pt.mmkamei.springsecurityjwt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pt.mmkamei.springsecurityjwt.model.User;
+import pt.mmkamei.springsecurityjwt.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
+
